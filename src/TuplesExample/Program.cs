6e6a9a3ff41fe0e4a7f6066c18c26e32917a1ee4
Mark Arteaga <mark@redbitdev.com>
2021-08-22 11:43:21 -0400

    helper method with out parameters

--- src/TuplesExample/Program.cs
+++ src/TuplesExample/Program.cs
@@ -6,8 +6,30 @@ namespace TuplesExample
     {
         static void Main()
         {
+            // The CIF to parse
             var cif = "0000111122222";
 
+            // the variables to set
+            string yearId, officeId, caseId;
+
+            // call the method to parse
+            ParseCif(cif, out yearId, out officeId, out caseId);
+
+            // output to console for now
+            Console.WriteLine($"Year Id: {yearId}");
+            Console.WriteLine($"Office Id: {officeId}");
+            Console.WriteLine($"Case Id: {caseId}");
+        }
+
+        /// <summary>
+        /// Parse the CIF and set the values of year, office and case parameters passed in
+        /// </summary>
+        /// <param name="cif"></param>
+        /// <param name="yearId"></param>
+        /// <param name="officeId"></param>
+        /// <param name="caseId"></param>
+        private static void ParseCif(string cif, out string yearId, out string officeId, out string caseId)
+        {
             // make sure it's not null
             if (string.IsNullOrWhiteSpace(cif))
             {
@@ -20,13 +42,10 @@ namespace TuplesExample
                 throw new ArgumentException($"CIF value {cif} is length of {cif.Length} but should be 13");
             }
 
-            var yearId = cif.Substring(0, 4);
-            var officeId = cif.Substring(4, 4);
-            var caseId = cif.Substring(8, 5);
-
-            Console.WriteLine($"Year Id: {yearId}");
-            Console.WriteLine($"Office Id: {officeId}");
-            Console.WriteLine($"Case Id: {caseId}");
+            // parse the data
+            yearId = cif.Substring(0, 4);
+            officeId = cif.Substring(4, 4);
+            caseId = cif.Substring(8, 5);
         }
     }
 }
