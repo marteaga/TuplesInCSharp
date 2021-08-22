637e9602754e3d1354b1c4a38217a4d7402413ed
Mark Arteaga <mark@redbitdev.com>
2021-08-22 11:35:16 -0400

    adding exception handling

--- src/TuplesExample/Program.cs
+++ src/TuplesExample/Program.cs
@@ -4,9 +4,22 @@ namespace TuplesExample
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             var cif = "0000111122222";
+
+            // make sure it's not null
+            if (string.IsNullOrWhiteSpace(cif))
+            {
+                throw new ArgumentException($"Cannot parse CIF if it's empty or null");
+            }
+
+            // make sure the length is 13
+            if (cif.Length != 13)
+            {
+                throw new ArgumentException($"CIF value {cif} is length of {cif.Length} but should be 13");
+            }
+
             var yearId = cif.Substring(0, 4);
             var officeId = cif.Substring(4, 4);
             var caseId = cif.Substring(8, 5);
