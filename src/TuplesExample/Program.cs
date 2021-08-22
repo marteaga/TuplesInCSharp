70204ea1e3507c3daeb9a57506bf4b366443300c
Mark Arteaga <mark@redbitdev.com>
2021-08-22 11:28:17 -0400

    initial example of parsing code

--- src/TuplesExample/Program.cs
+++ src/TuplesExample/Program.cs
@@ -6,7 +6,14 @@ namespace TuplesExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cif = "0000111122222";
+            var yearId = cif.Substring(0, 4);
+            var officeId = cif.Substring(4, 4);
+            var caseId = cif.Substring(8, 5);
+
+            Console.WriteLine($"Year Id: {yearId}");
+            Console.WriteLine($"Office Id: {officeId}");
+            Console.WriteLine($"Case Id: {caseId}");
         }
     }
 }
